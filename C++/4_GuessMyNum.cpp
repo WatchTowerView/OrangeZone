fc800cdc885b503b754fe11011b2e36f47af1ec9
sajmaxx <sajan.mathews75@gmail.com>
2018-10-03 22:01:57 -0400

    added cplusplus

---
+++ C++/4_GuessMyNum.cpp
@@ -0,0 +1,40 @@
+// 4_GuessMyNum.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int answer = 7;
+	int k;
+
+	bool keeplooking = true;
+
+	while (keeplooking)
+	{
+		cout << "Enter a number of your choice! ";
+
+		cin >> k;
+
+		if (k > answer)
+		{
+			cout << " Hey you are a bit high, try lower!" << endl;
+		}
+		else if (k < answer)
+		{
+			cout << " Hey your number is a bit low, try higher!" << endl;
+		}
+		else
+		{
+			cout << "CONGRATS  you got it!!!" << endl;
+			keeplooking = false;
+		}
+
+	}
+    return 0;
+}
+
