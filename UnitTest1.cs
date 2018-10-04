815b456948f302ae3e3929170620a648565f944d
sajmaxx <sajan.mathews75@gmail.com>
2018-10-03 21:54:21 -0400

    do new files

---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SajUnitTest;
+
+namespace SajTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AbCStart bab = new AbCStart();
+
+            var cc = 100;
+            //Assert.AreEqual(cc , bab.toto);
+        }
+
+
+        [TestMethod]
+        public void SajaIsRight()
+        {
+            AbCStart cac = new AbCStart();
+
+            double  dd = 30;
+
+            var cc = dd + 30;
+
+            dd = dd + 30;
+            Assert.AreEqual(dd, cc); // cac.toto);
+
+
+        }
+    }
+}
