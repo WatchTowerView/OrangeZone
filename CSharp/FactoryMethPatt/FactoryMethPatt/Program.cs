c4efa365c26c555d133f409e59fdfedde8c45c8f
SajMaxx <sajan.mathews75@gmail.com>
2018-12-02 20:09:56 -0500

    added factory method pattern example

---
+++ CSharp/FactoryMethPatt/FactoryMethPatt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethPatt
+{
+
+
+    public class Point
+    {
+        //Factory Method
+        public static Point PointWithCartes(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        private double x, y;
+        public Point()
+        {
+        }
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+    }
+
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
